fcbd90a5a995cc34ff430eb5b10a591806e3eaf0
Drek1Droid <derekburney@live.com>
2014-09-26 15:10:01 -0700

    Dynamic Location calc, and location menu items reflect state

--- js/controllers.js
+++ js/controllers.js
@@ -7,6 +7,8 @@ app.controller('JoynerApp_DeviceController', ['$scope', 'dataService', 'azureDat
         $scope.ascending = true;
         $scope.currentColumn = 0;
         $scope.menu = "main";
+        $scope.locationList = [];
+        $scope.numLocations = 0;
 
         //function to intialize the connection
         $scope.initialize = function () {
@@ -14,7 +16,11 @@ app.controller('JoynerApp_DeviceController', ['$scope', 'dataService', 'azureDat
             // dataService.addSource(azureDataService);
             dataService.addSource(nitrogenDataService);
             nitrogenDataService.connect('test@nitrogen.io', 'DxTest12', function (err) {
-                dataService.connect(function (results) { }, $scope, 3);
+                dataService.connect(function (results) {
+                    if ($scope.devices) {
+                        $scope.GetLocations();
+                    }
+                }, $scope, 3);
             });
         }
 
@@ -24,14 +30,39 @@ app.controller('JoynerApp_DeviceController', ['$scope', 'dataService', 'azureDat
             $scope.currentDeviceID = device.id;
         }
 
-        $scope.FilterLocation = function (network) {
+        $scope.FilterLocation = function (network, index) {
             if ($scope.selectedNetwork == network || $scope.selectedNetwork == 'All') {
                 $scope.selectedNetwork = '';
-                console.log("removing filter" + network);
+                $scope.ShowActiveLocation(index, false);
             }
             else {
                 $scope.selectedNetwork = network;
-                console.log("adding filter" + network);
+                $scope.ShowActiveLocation(index, true);
+            }
+        }
+
+        $scope.ShowActiveLocation = function (index, active) {
+            // go through all the filter items and set them to neutral
+            for (var i = 0; i < $scope.numLocations; i++) {
+                document.getElementById("loc" + i).style.backgroundColor = "";
+            }
+            if (active) {
+                // show the active filter in some meaningful way - the current method chews - TBS make UI look good
+                document.getElementById("loc" + index).style.backgroundColor = "#000000";
+            }
+        }
+
+        $scope.GetLocations = function () {
+            var unique = {};
+            $scope.locationList = [];
+            console.log("Device Table Length: " + $scope.devices.length);
+            for (var i = 0; i < $scope.devices.length; i++) {
+                if (typeof unique[$scope.devices[i]["network"]] == "undefined") {
+                    unique[$scope.devices[i]["network"]] = "";
+                    $scope.locationList.push($scope.devices[i]);
+                    $scope.numLocations = $scope.locationList.length;
+                    console.log("There are " + $scope.numLocations + " locations.")
+                }
             }
         }
 
