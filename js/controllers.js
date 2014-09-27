9fc6cc9631c0c0b0cff1371944fca257c3f12bf4
Derek Burney <derekburney@live.com>
2014-09-26 21:54:51 -0700

    Removed unused code for location filters

--- js/controllers.js
+++ js/controllers.js
@@ -13,7 +13,6 @@ app.controller('JoynerApp_DeviceController', ['$scope', 'dataService', 'azureDat
         //function to intialize the connection
         $scope.initialize = function () {
             azureDataService.addSource('https://joyner.azure-mobile.net/', 'lDfAwKeKBLQFCckQNxkOSGaUiukSfe41', ['devices']);
-            // dataService.addSource(azureDataService);
             dataService.addSource(nitrogenDataService);
             nitrogenDataService.connect('test@nitrogen.io', 'DxTest12', function (err) {
                 dataService.connect(function (results) {
@@ -33,38 +32,22 @@ app.controller('JoynerApp_DeviceController', ['$scope', 'dataService', 'azureDat
         $scope.FilterLocation = function (network, index) {
             if ($scope.selectedNetwork == network || network == 'All') {
                 $scope.selectedNetwork = '';
-                $scope.ShowActiveLocation(index, false);
             }
             else {
                 $scope.selectedNetwork = network;
-                $scope.ShowActiveLocation(index, true);
             }
         }
 
-        $scope.ShowActiveLocation = function (index, active) {
-            return;
-            // go through all the filter items and set them to neutral
-            for (var i = 0; i < $scope.numLocations; i++) {
-                document.getElementById("loc" + i).style.backgroundColor = "";
-            }
-            if (active) {
-                // show the active filter in some meaningful way - the current method chews - TBS make UI look good
-                document.getElementById("loc" + index).style.backgroundColor = "#000000";
-            }
-        }
 
         $scope.GetLocations = function () {
             var unique = {};
             $scope.locationList = [];
             $scope.locationList.push("All");
-            console.log("Device Table Length: " + $scope.devices.length);
             for (var i = 0; i < $scope.devices.length; i++) {
                 if (typeof unique[$scope.devices[i]["network"]] == "undefined") {
                     unique[$scope.devices[i]["network"]] = "";
-                    //$scope.locationList.push($scope.devices[i]);
                     $scope.locationList.push($scope.devices[i].network);
                     $scope.numLocations = $scope.locationList.length;
-                    console.log("There are " + $scope.numLocations + " locations.")
                 }
             }
         }
